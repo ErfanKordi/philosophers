2e6c681932eb11f539b6bc89a8b139a05c3129bf
Erfan <erfan.12.kordi@gmail.com>
2023-10-05 13:07:19 +0200

    first

---
+++ philo.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekordi <ekordi@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 12:20:29 by ekordi            #+#    #+#             */
+/*   Updated: 2023/10/04 04:21:58 by ekordi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 4)
+	{
+		ft_putstr_fd("too few arguments!", 2);
+		exit(1);
+	}
+	int number_of_philosophers = ft_atoi(argv[1]);
+	pthread_t *philo = (pthread_t*)calloc(sizeof(pthread_t), number_of_philosophers);
+	int forks =  number_of_philosophers;
+	
+}
---
+++ philo.h
@@ -0,0 +1,11 @@
+#ifndef PHILO_H
+#define	PHILO_H
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include "My_C_Library/libft.h"
+
+
+
+#endif
